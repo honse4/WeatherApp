60e5bdf6062f692cea81285535e573cdf406de7f
Vasav Juyal <vasavj123@gmail.com>
2024-04-06 13:47:18 +0300

    components: Back button and vbox for favourites and ui changes
    Back button to go back to the weather view, vbox for the future part and
    some ui changes. Also, api call is made into its own thread to not hang
    up the main thread. Also works to add a loading part.

--- WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/components/SearchBar.java
+++ WeatherApp/src/main/java/fi/tuni/prog3/weatherapp/components/SearchBar.java
@@ -1,16 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package fi.tuni.prog3.weatherapp.components;
 
 import fi.tuni.prog3.weatherapp.WeatherApp;
+import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -23,39 +21,77 @@ public class SearchBar extends VBox {
     private final Stage stage;
     private final Scene scene;
     private final WeatherApp main;
+    private final TextField searchbar;
+    private final Label error;
+    private final HBox loading ;
     
     public SearchBar(Stage stage, Scene scene, WeatherApp main) {
         this.stage = stage;
         this.scene = scene;
         this.main = main;
+        this.searchbar = new TextField();
+        this.error = new Label("");
+        this.loading = new HBox(new Label("Loading..."));
+
+        loading.setAlignment(Pos.CENTER);
         
-        getChildren().add(createNewScene());
-    }
-    
-    private VBox createNewScene() {
-        
-        TextField searchbar = new TextField();
         searchbar.setMinSize(300,30);
-        searchbar.setStyle("-fx-border-radius: 20;");
+        searchbar.setStyle("-fx-border-color: transparent;");
         
-        Label error = new Label("");
         error.setAlignment(Pos.CENTER);
         error.setMinWidth(200);
         
-        VBox topRow = createTopRow(searchbar,
-                getSearchButton(searchbar, error), error);
+        
+        getChildren().addAll(getBackButton(), getVBox(),getFavourites());
+        setSpacing(50);
+        
+        setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                onClick();
+            }
+        });
+    }
+    
+    private Button getBackButton() {
+        Button back = new Button("<-");
+        back.setFocusTraversable(false);
+        back.setMinWidth(40);
+        
+        back.setOnAction(e -> {
+            searchbar.setText("");
+            error.setText("");
+            searchbar.requestFocus();
+            stage.setScene(scene);
+            scene.getRoot().requestFocus();    
+        });
+        VBox.setMargin(back, new Insets(0, -50,0, 0));
+        return back;
+    }
+    
+    private VBox getVBox() {
+        Button searchButton = new Button();
+        searchButton.setMinSize(50,30);
+        
+        searchButton.setOnAction(e -> {
+            onClick();
+        });
+        
+        VBox topRow = getTopRow(searchbar, searchButton, error);
         
         VBox container = new VBox(topRow);
-        VBox.setMargin(topRow, new Insets(50, 0, 0, 0));
+        VBox.setMargin(topRow, new Insets(20, 0, 0, 0));
         container.setSpacing(10);
         
         return container;
     }
     
-    private VBox createTopRow(TextField searchbar, Button searchButton, Label error) {
+    private VBox getTopRow(TextField searchbar, Button searchButton, Label error) {
         HBox searchRow = new HBox(searchbar, searchButton);
         searchRow.setSpacing(0);
         searchRow.setAlignment(Pos.CENTER);
+        searchRow.setStyle("-fx-border-color: #a0a0a0;"
+                + "-fx-border-radius: 20; -fx-padding: 5; -fx-border-width: 1;");
+        searchRow.setMaxWidth(400);
         
         VBox topRow = new VBox(searchRow, error);
         topRow.setSpacing(5);
@@ -64,26 +100,41 @@ public class SearchBar extends VBox {
         return topRow;
     }
     
-    private Button getSearchButton(TextField searchbar, Label error) {
-        Button searchButton = new Button();
-        searchButton.setMinSize(50,30);
+    private void onClick() {
         
-        searchButton.setOnAction(e -> {
-            String search = searchbar.getText();
-            searchbar.setText("");
-            error.setText("");
-            boolean value = main.searchResult(search);   
-            
-            if (value) {
-                searchbar.requestFocus();
-                stage.setScene(scene);
-                scene.getRoot().requestFocus();
+        String search = searchbar.getText();
+        searchbar.setText("");
+        error.setText("");
+        
+        getChildren().add(2, loading);
+        
+        //ai
+        Task<Boolean> task = new Task<Boolean>() {
+            @Override
+            protected Boolean call() throws Exception {
+                return main.searchResult(search); 
             }
-            else {   
-                error.setText("Place Does Not Exist");
-            }    
-        });
-        
-        return searchButton;
+
+            @Override
+            protected void succeeded() {
+                Boolean result = getValue();
+                if (result) {
+                    searchbar.requestFocus();
+                    stage.setScene(scene);
+                    scene.getRoot().requestFocus();
+                } else {
+                    error.setText("Place Does Not Exist");
+                }
+                getChildren().remove(loading);
+            }
+        };
+        new Thread(task).start();   
+    }
+    
+    private VBox getFavourites() {
+        Label title = new Label("Favourites");
+        VBox favourites = new VBox(title);
+        favourites.setAlignment(Pos.CENTER);
+        return favourites;
     }
 }
